884c4bb3522c07e3390bb5f48a950d39a52c2838
Sergey <38881254+sergey-goncharenko@users.noreply.github.com>
2025-05-03 18:18:25 +0300

    added some debug

--- bridge.py
+++ bridge.py
@@ -257,6 +257,7 @@ try:
                 raise Exception("Could not connect to cec adapter")
         except Exception as e:
             print("ERROR: Could not initialise CEC:", str(e))
+            print(f"  CEC: {config['cec']['port']}")
             exit(1)
 
     ### Setup IR ###
